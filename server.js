f768062474e99303af98cdacba65b2552c3f0896
Cullan Shewfelt <cullanrocks@gmail.com>
2019-08-07 13:12:45 -0700

    trying one more time.

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ util.inspect.defaultOptions.maxArrayLength = null;
 // use this for development builds (prod at bottom) :
 // *****************************************************************************************
 
-// app.use(express.json());
-// app.use(express.urlencoded({extended: true})); // extended true means you can use nested objects in post requests
-// app.use(cors({credentials: true, origin: true}));
-// app.options('*', cors())
-// app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({extended: true})); // extended true means you can use nested objects in post requests
+app.use(cors({credentials: true, origin: true}));
+app.options('*', cors())
+app.use(compression());
 
 // *****************************************************************************************
 
@@ -33,8 +33,8 @@ app.use('/move/', linksRouter);
 app.use(express.static(path.join(__dirname, 'public/build')));
 
 app.get('/*', (req, res) => {
-  // res.header("Access-Control-Allow-Origin", "*");
-  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.sendFile(path.join(__dirname + '/public/index.html'));
 })
 
