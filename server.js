9f8b16f6cc77fdcba7da17a1d1d1434c787798d8
Cullan Shewfelt <cullanrocks@gmail.com>
2019-08-07 12:32:00 -0700

    fixed server.js reintroduced env variable.

--- server.js
+++ server.js
@@ -7,15 +7,16 @@ const independentArtistsRouter = require('./routes/independent-artists-routes');
 const exportsRouter = require('./routes/exports-routes');
 const uploadsRouter = require('./routes/uploads');
 const linksRouter = require('./routes/linkRoutes');
-
-
+const compression = require('compression');
 const app = express();
 
-const util = require('util');
-
-const compression = require('compression');
+const port = process.env.PORT || 4000;
 
 util.inspect.defaultOptions.maxArrayLength = null;
+
+// *****************************************************************************************
+// use this for development builds (prod at bottom) :
+// *****************************************************************************************
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true})); // extended true means you can use nested objects in post requests
@@ -24,25 +25,23 @@ app.options('*', cors())
 app.use(compression());
 app.use(express.static(path.join(__dirname, 'public/build')));
 
-app.get('/', (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.sendFile(path.join(__dirname + '/public/index.html'));
-  next();
-})
-
 // app.use('/api/background-instrumentals/', backgroundInstrumentalsRouter);
 app.use('/api/independent-artists/', independentArtistsRouter);
 app.use('/api/exports/', exportsRouter);
 app.use('/upload/', uploadsRouter);
 app.use('/move/', linksRouter);
 
-
+app.get('/*', (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+  next();
+})
 
 
 // ********************************************************************************************************************************
 
-app.listen(4000, () => {
+app.listen(port, () => {
   console.log('listening on port 4000');
 })
 
