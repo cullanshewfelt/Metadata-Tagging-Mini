e585d11b502256c74c11921a0b2ef363df49b71f
Cullan Shewfelt <cullanrocks@gmail.com>
2019-08-08 15:06:00 -0700

    Minor change.

--- connection.js
+++ connection.js
@@ -1,17 +1,17 @@
-const mysql =  require('mysql');
-const env = require('dotenv').config();
+const mysql =  require("mysql");
+const env = require("dotenv").config();
 let indieDB;
 
 if(process.env.JAWSDB_URL){
-  indieDB = mysql.createPool(process.env.JAWSDB_URL)
+  indieDB = mysql.createPool(process.env.JAWSDB_URL);
 } else {
-    indieDB = mysql.createPool({
-      host     : env.DB_HOST,
-      port     : 8889,
-      user     : env.DB_USER,
-      password : env.DB_PASS,
-      database : 'dlmusic_artist'
-    })
+  indieDB = mysql.createPool({
+    host     : env.DB_HOST,
+    port     : 8889,
+    user     : env.DB_USER,
+    password : env.DB_PASS,
+    database : "dlmusic_artist"
+  });
 }
 
 module.exports = indieDB;
