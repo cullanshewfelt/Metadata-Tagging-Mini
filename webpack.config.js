95efb916dd26f34444d659ba579d5377bb73dfac
Cullan Shewfelt <cullanrocks@gmail.com>
2019-08-08 10:44:01 -0700

    Adding multiple webpack configurations. Adding eslint.

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,61 @@
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const path = require('path');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const path = require("path");
 
 module.exports = (env) => {
-  const isProduction = env === 'production';
-  console.log('env', env)
+  const isProduction = env === "production";
+  console.log("env", env)
   return {
-    entry: path.join(__dirname, '/src/app.js'),
+    entry: path.join(__dirname, "/src/app.js"),
     output: {
-      path: path.join(__dirname, 'public/build'),
-      filename: 'bundle.js'
+      path: path.join(__dirname, "public/build"),
+      filename: "bundle.js"
     },
-    mode: 'none',
+    mode: "none",
     module: {
       rules: [{
-        include: path.resolve(__dirname, 'src'),
-        loader: 'babel-loader',
+        include: path.resolve(__dirname, "src"),
+        loader: "babel-loader",
         test: /\.js$/,
         exclude: /node_modules/
       },{
         test: /\.s?css$/,
         use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
+          "style-loader",
+          "css-loader",
+          "sass-loader"
         ]
       },{
         test: /\.(png|jpe?g|gif)$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
               name(file) {
-                if (process.env.NODE_ENV === 'development') {
-                  return '[path][name].[ext]';
+                if (process.env.NODE_ENV === "development") {
+                  return "[path][name].[ext]";
                 }
-                return '[hash].[ext]';
+                return "[hash].[ext]";
               },
             }
           }
         ]
+      },{
+       test: /\.js$/,
+       exclude: /node_modules/,
+       loader: "eslint-loader",
+       options: {
+         // eslint options (if necessary)
+       }
       }]
     },
-    devtool: isProduction ? 'source-map' : 'cheap-module-source-map',
+    devtool: isProduction ? "source-map" : "cheap-module-source-map",
     devServer: {
-      contentBase: path.join(__dirname, 'public'),
+      contentBase: path.join(__dirname, "public"),
       historyApiFallback: true,
       hot: true
     },
     resolve: {
-      modules: [path.resolve(__dirname, 'app'), 'node_modules']
+      modules: [path.resolve(__dirname, "app"), "node_modules"]
     },
     optimization: {
       minimizer: [new UglifyJsPlugin()]
